c18006d16fa2444274597d7d1e6cc6bf9568caf7
hyonie <dev.hyonie@gmail.com>
2023-04-15 16:35:00 +0900

    feat: 프로젝트 레이아웃

--- client/src/components/ProjectsSection.tsx
+++ client/src/components/ProjectsSection.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { getAllProject } from "@/service/projects";
+import Image from "next/image";
 
 export default async function ProjectsSection() {
   const projects = await getAllProject();
+
   return (
     <section id="projects">
       <h1 className="text-center font-bold text-3xl">
@@ -10,7 +12,26 @@ export default async function ProjectsSection() {
         <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded" />
       </h1>
 
-      <div className="flex flex-col space-y-28">{}</div>
+      <div className="flex flex-col space-y-28">
+        {projects.map((project, index) => {
+          return (
+            <div key={index}>
+              <div className="flex flex-col md:flex-row">
+                <div className="md:w-1/2">
+                  <Image
+                    className="rounded-xl "
+                    src={`/images/projects/${project.path}.png`}
+                    alt={project.path}
+                    width={1000}
+                    height={1000}
+                  />
+                </div>
+                <div></div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 }
