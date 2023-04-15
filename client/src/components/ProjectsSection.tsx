040e432327dee53e13a67002292270d315af312d
hyonie <dev.hyonie@gmail.com>
2023-04-15 16:44:46 +0900

    feat: 프로젝트 레이아웃2

--- client/src/components/ProjectsSection.tsx
+++ client/src/components/ProjectsSection.tsx
@@ -16,17 +16,22 @@ export default async function ProjectsSection() {
         {projects.map((project, index) => {
           return (
             <div key={index}>
-              <div className="flex flex-col md:flex-row">
+              <div className="flex flex-col md:flex-row md:space-x-12">
                 <div className="md:w-1/2">
                   <Image
-                    className="rounded-xl "
+                    className="rounded-xl shadow-xl hover:opacity-70"
                     src={`/images/projects/${project.path}.png`}
                     alt={project.path}
                     width={1000}
                     height={1000}
                   />
                 </div>
-                <div></div>
+                <div className="md:1/2">
+                  <h1 className="text-3xl font-bold mb-6">{project.title}</h1>
+                  <p className="text-xl leading-7 mb-4 text-neutral-600">
+                    {project.description}
+                  </p>
+                </div>
               </div>
             </div>
           );
