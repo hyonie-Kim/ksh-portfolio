522a1b5709d46aeed598f4dd7b4671c26a1258c0
hyonie <dev.hyonie@gmail.com>
2024-07-30 00:03:10 +0900

    chore: base url env 파일 추가

--- src/service/contact.ts
+++ src/service/contact.ts
@@ -1,17 +1,20 @@
-import { EmailData } from "./email";
+import { EmailData } from './email';
 
 export async function sendContactEmail(email: EmailData) {
-  const response = await fetch("/api/contact", {
-    method: "POST",
-    body: JSON.stringify(email),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/contact`,
+    {
+      method: 'POST',
+      body: JSON.stringify(email),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
 
   const data = await response.json();
   if (!response.ok) {
-    throw new Error(data.message || "서버 요청 실패");
+    throw new Error(data.message || '서버 요청 실패');
   }
   return data;
 }
