c090a137447df6d0ea596479107bb0af59474892
hyonie <hyonie@annextele.com>
2025-09-01 17:13:41 +0900

    Style: 프로젝트 slug 동적 세크먼트 추가

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -19,8 +19,10 @@ export default async function ProjectPage({ params: { slug } }: Props) {
   const project = await getProjectData(slug);
 
   return (
-    <article>
-      <h1>{project.title}</h1>
+    <article className="mx-auto px-4 sm:px-6 md:px-8 max-w-3xl md:max-w-4xl lg:max-w-4xl">
+      {/* <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold tracking-tight mt-8 md:mt-10">
+        {project.title}
+      </h1> */}
       <MarkdownViewer content={project.content} />
     </article>
   );
