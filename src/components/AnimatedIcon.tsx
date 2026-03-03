b074999e35a2b282abd0e4c9c706ba6d4ce61d10
Hyonie <dev.hyonie@gmail.com>
2026-03-03 23:30:44 +0900

    Refactor: 메일발송

--- src/components/AnimatedIcon.tsx
+++ src/components/AnimatedIcon.tsx
@@ -9,16 +9,18 @@ import {
   SiInstagram,
 } from 'react-icons/si';
 import { link } from 'fs';
+import { Link as ScrollLink } from 'react-scroll';
 const LINKS = [
-  { icon: <SiGithub />, url: 'https://github.com/hyonie-Kim', label: '깃허브' },
-  { icon: <SiGmail />, url: 'mailto:dev.hyonie@gmail.com', label: '이메일' },
+  { icon: <SiGithub />, type:'external', url: 'https://github.com/hyonie-Kim', label: '깃허브' },
+  { icon: <SiGmail />, type:'scroll', to: 'contact', label: '이메일' },
   {
     icon: <SiTistory />,
+    type:'external',
     url: 'https://dev-hyonie.tistory.com/',
     label: '블로그'
   },
 
-  { icon: <SiInstagram />, url: 'https://www.instagram.com/hyonie.develop/', label: '인스타그램' },
+  { icon: <SiInstagram />, type:'external', url: 'https://www.instagram.com/hyonie.develop/', label: '인스타그램' },
   // {
   //   icon: <SiNaver />,
   //   url: "https://blog.naver.com/star_loo4/",
@@ -33,16 +35,36 @@ export default function AnimatedIcon() {
       <ul className="flex gap-4 my-2">
         {LINKS.map((link, index) => {
 
-          const isMailto = link.url.startsWith('mailto:');
-          
+          if(link.type  === 'scroll'){
+
+            return (
+              <motion.div key={index} 
+              className="w-5 mx-3 hover:text-yellow-400 sm:mx-1"
+              whileHover={{ y: 2 }}
+              whileTap={{ scale: 0.9 }}
+              >
+                <ScrollLink
+                to={link.to || ''}
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+                aria-label={link.label}
+                title={link.label}
+                >
+                  {link.icon}
+                </ScrollLink>
+              </motion.div>
+            )
+          }
 
           return (
             <motion.a
             className="w-5 mx-3 hover:text-yellow-400 sm:mx-1"
             key={index}
             href={link.url}
-            target={isMailto ? undefined : '_blank'}
-            rel={isMailto ? undefined : 'noreferrer'}
+            target='_blank'
+            rel='noopener noreferrer'
             whileHover={{ y: 2 }}
             whileTap={{ scale: 0.9 }}
             aria-label={link.label}
